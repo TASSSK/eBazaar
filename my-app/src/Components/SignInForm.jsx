b2882068b19b7b6d10f72f6d50ab212a517334bb
purushendra <purushendrakarn@gmail.com>
2023-12-10 15:39:18 +0530

    login functionality changes with favicon and title

--- my-app/src/Components/SignInForm.jsx
+++ my-app/src/Components/SignInForm.jsx
@@ -1,15 +1,31 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Form from 'react-bootstrap/Form';
-import "./SignForm.css"
-import logoo from "./Assets/arrow.png"
+import "./SignForm.css";
+import logoo from "./Assets/arrow.png";
 
 const SignInForm = () => {
+  const navigate = useNavigate(); // Access the navigate function
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [showFailed, setShowFailed] = useState(false);
 
   const handleSignIn = () => {
-    console.log("Email: " + email);
-    console.log("Password: " + password);
+    if (email === 'purushendrakarn@gmail.com' && password === 'password') {
+      console.log("Login successful!");
+      setLoggedIn(true);
+      setShowSuccessMessage(true);
+      setShowFailed(false);
+      // Redirect to homepage on successful login
+      navigate("/homepage"); // Update "/homepage" with your actual homepage route
+    } else {
+      console.log("Invalid credentials");
+      setLoggedIn(false);
+      setShowSuccessMessage(false);
+      setShowFailed(true);
+    }
   };
 
   const handleEmailChange = (e) => {
@@ -32,9 +48,16 @@ const SignInForm = () => {
           <label>Password</label>
           <input type="password" required onChange={handlePasswordChange} />
         </div>
-        <button className="signin-button" onClick={handleSignIn}> SIGN IN
-          <div className="logocontainer" ><img src={logoo} width={20} height={20} /></div>
+        <button className="signin-button" onClick={handleSignIn}>
+          {loggedIn ? 'Logged In' : 'SIGN IN'}
+          <div className="logocontainer" ><img src={logoo} width={20} height={20} alt="arrow" /></div>
         </button>
+        {showSuccessMessage && (
+          <p className="success-message">Login successful! Redirecting...</p>
+        )}
+        {showFailed && (
+          <p className="failed-message">Invalid credentials. Please try again.</p>
+        )}
       </Form>
     </div>
   );
